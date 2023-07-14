44491196552071f01952b47c5c7c6550cb436cee
Andy Stewart <andy.stewart@live.com>
2023-07-14 15:20:47 +1000

    Update const.py

--- const.py
+++ const.py
@@ -1,7 +1,7 @@
 """Constants for qBittorrent."""
 from typing import Final
 
-DOMAIN: Final = "qbittorrent"
+DOMAIN: Final = "qbittorrent-ha"
 
 DEFAULT_NAME = "qBittorrent"
 DEFAULT_URL = "http://127.0.0.1:8080"
