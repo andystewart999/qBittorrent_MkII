4f694c2f7d68a67b9d5c73d35b2d5d4224b8998a
Andy Stewart <andy.stewart@live.com>
2023-07-31 07:37:27 +1000

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,11 +1,106 @@
 """Helper functions for qBittorrent."""
 from qbittorrent.client import Client
+from .const import *
+import json
 
+all_torrents_prev = []
 
 def setup_client(url: str, username: str, password: str, verify_ssl: bool) -> Client:
     """Create a qBittorrent client."""
-    client = Client(url, verify=verify_ssl)
+    client = Client(url, verify=verify_ssl, timeout=10)
     client.login(username, password)
     # Get an arbitrary attribute to test if connection succeeds
     client.get_alternative_speed_status()
     return client
+
+
+def find_torrent(torrent_list, default=None):
+    for x in torrent_list:
+        return x
+    return default
+
+
+def compare_torrents(client: Client):
+    #Return a dict containing all changed torrents and their new state
+    global all_torrents_prev
+    
+    completed_torrents= {}
+    added_torrents= {}
+    removed_torrents = {}
+
+    #Retrieve current torrent info    
+    all_torrents = client.torrents()
+
+    #Cater for a fresh startup - we don't want to raise a ton of events straight away
+    if all_torrents_prev:
+        for torrent in all_torrents_prev:
+            #See if each torrent is still in the new list
+            #If not, it's been deleted
+            #If so, check the completion_on value - has it finished?
+            prev_name = torrent['name']
+            prev_hash = torrent['hash']
+            prev_completion_on = torrent['completion_on']
+
+            found_torrent = find_torrent(x for x in all_torrents if x['hash'] == prev_hash)
+            if found_torrent is not None:
+                if found_torrent['completion_on'] != prev_completion_on:
+                    #See if it's just completed downloading
+                    if found_torrent['completion_on'] > 0:
+                        #It's just finished downloading
+                        completed_torrents[prev_hash] = prev_name
+            else:
+                removed_torrents[prev_hash] = prev_name
+
+        for torrent in all_torrents:
+            #See if any new torrents have been added - it won't be in the previous list
+            name = torrent['name']
+            hash = torrent['hash']
+            new_torrent = find_torrent(x for x in all_torrents_prev if x['hash'] == hash)
+            if new_torrent is None:
+                added_torrents[hash] = name
+
+    all_torrents_prev = all_torrents
+ 
+    return completed_torrents, added_torrents, removed_torrents
+
+def pause_downloads(client: Client):
+    client.pause_all()
+    return
+
+def resume_downloads(client: Client):
+    client.resume_all()
+    return
+
+def get_torrent_info(client: Client, hash: str):
+    if hash != '' and hash != 'all':
+        try:
+            torrent = client.get_torrent(hash)
+            return torrent
+        except Exception as err:
+            return {}
+    else:
+        torrents = client.torrents()
+        return {
+            "torrents": [
+                {
+                    "hash": torrent["hash"],
+                    "name": torrent["name"],
+                    "addition_date": torrent["addition_date"],
+                    "completion_date": torrent["completion_date"],
+                    "eta": torrent["eta"],
+                    "peers": torrent["peers"],
+                    "save_path": torrent["save_path"],
+                    "seeds": torrent["seeds"],
+                    "total_downloaded": torrent["total_downloaded"],
+                    "total_size": torrent["total_size"]
+                } for torrent in torrents
+            ],
+        }
+
+def shutdown(client: Client):
+    try:
+        client.shutdown()
+    except Exception as err:
+        LOGGER.error(err)
+        pass
+    return
