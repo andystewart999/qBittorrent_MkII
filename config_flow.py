c16908f8f0c9f0eea3dd06a3241486b464e926fe
Andy Stewart <andy.stewart@live.com>
2023-07-31 07:35:51 +1000

    Update config_flow.py

--- config_flow.py
+++ config_flow.py
@@ -1,7 +1,7 @@
 """Config flow for qBittorrent."""
 from __future__ import annotations
 
-import logging
+#import logging
 from typing import Any
 
 from qbittorrent.client import LoginRequired
@@ -14,16 +14,14 @@ from homeassistant.const import (
     CONF_PASSWORD,
     CONF_URL,
     CONF_USERNAME,
-    CONF_VERIFY_SSL
+    CONF_VERIFY_SSL,
+    CONF_SCAN_INTERVAL,
 )
 from homeassistant.data_entry_flow import FlowResult
 
 #from .const import DEFAULT_NAME, DEFAULT_URL, DOMAIN
 from .const import *
-
 from .helpers import setup_client
-
-_LOGGER = logging.getLogger(__name__)
 
 USER_DATA_SCHEMA = vol.Schema(
     {
@@ -31,14 +29,16 @@ USER_DATA_SCHEMA = vol.Schema(
         vol.Required(CONF_USERNAME): str,
         vol.Required(CONF_PASSWORD): str,
         vol.Optional(CONF_VERIFY_SSL, default=False): bool,
+        vol.Required(CONF_SCAN_INTERVAL, default=DEFAULT_SENSOR_SCAN_INTERVAL): int,
     }
 )
 
 USER_EVENTS_SCHEMA = vol.Schema(
     {
-        vol.Required(CONF_EVENT_COMPLETE, default=DEFAULT_EVENT_COMPLETE): bool,
-        vol.Required(CONF_EVENT_ADDED, default=DEFAULT_EVENT_ADDED): bool,
-        vol.Required(CONF_EVENT_REMOVED, default=DEFAULT_EVENT_REMOVED): bool,
+        vol.Optional(CONF_EVENT_COMPLETE, default=DEFAULT_EVENT_COMPLETE): bool,
+        vol.Optional(CONF_EVENT_ADDED, default=DEFAULT_EVENT_ADDED): bool,
+        vol.Optional(CONF_EVENT_REMOVED, default=DEFAULT_EVENT_REMOVED): bool,
+        vol.Required(CONF_EVENT_SCAN_INTERVAL, default=DEFAULT_EVENT_SCAN_INTERVAL): int,
     }
 )
 
@@ -67,17 +67,13 @@ class QbittorrentConfigFlow(ConfigFlow, domain=DOMAIN):
             except RequestException:
                 errors = {"base": "cannot_connect"}
             else:
-
+                #Save the user input
+                self.temp_user_input = user_input
+                
                 #Show the next page of the config flow
-                _LOGGER.error('Showing config flow: events')
                 return await self.async_step_events()
-                _LOGGER.error('After async_step_events')
-                #return self.async_create_entry(title=DEFAULT_NAME, data=user_input)
 
-        #schema = self.add_suggested_values_to_schema(USER_DATA_SCHEMA, user_input)
-        _LOGGER.error('Showing user form')
         return self.async_show_form(step_id="user", data_schema=USER_DATA_SCHEMA, errors=errors)
-        _LOGGER.error('After async_show_form user')
 
     async def async_step_events(
         self, user_input: dict[str, Any] | None = None
@@ -86,18 +82,16 @@ class QbittorrentConfigFlow(ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is not None:
-            self._async_abort_entries_match({CONF_URL: user_input[CONF_URL]})   #### what is this?   only just copied and pasted this section, lots to change still
+            #self._async_abort_entries_match({CONF_URL: user_input[CONF_URL]})   #### what is this?   only just copied and pasted this section, lots to change still
             
             #Just go straight to creating the entry
-            _LOGGER.error('async_create_entry')
+            #How do other multi-page integrations do this?
+            user_input = self.temp_user_input | user_input
+            
             return self.async_create_entry(title=DEFAULT_NAME, data=user_input)
-            _LOGGER.error('After async_create_entry')
 
-        #schema = self.add_suggested_values_to_schema(USER_EVENTS_SCHEMA, user_input)
-        _LOGGER.error('Showing event form')
         return self.async_show_form(step_id="events", data_schema=USER_EVENTS_SCHEMA, errors=errors)
-        _LOGGER.error('After async_show_form events')
-   
+
 
     async def async_step_import(self, config: dict[str, Any]) -> FlowResult:
         """Import a config entry from configuration.yaml."""
