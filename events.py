17c5d2114ee63e5ab6bd5166aac428a1d784c625
Andy Stewart <andy.stewart@live.com>
2023-08-09 09:27:36 +1000

    Update events.py
    Added 'configuration' options flow

--- events.py
+++ events.py
@@ -1,12 +1,11 @@
 from homeassistant.core import HomeAssistant, ServiceCall, ServiceResponse, SupportsResponse
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.event import async_track_time_interval
-from datetime import timedelta
+#from homeassistant.helpers.event import async_track_time_interval
+#from datetime import timedelta
 
 from qbittorrent.client import Client, LoginRequired
 from .const import *
 from .helpers import compare_torrents, pause_downloads, resume_downloads, get_torrent_info, shutdown
-
 
 class QBEventsAndServices:
     """Encapsulates logic for raising events based on torrent activity"""
@@ -14,18 +13,13 @@ class QBEventsAndServices:
     def __init__(
         self,
         hass: HomeAssistant,
-        entry: ConfigEntry,
+        config_entry: ConfigEntry,
     ):
         
         #Capture event yes/no info plus event timings
-        self.client = hass.data[DOMAIN][entry.entry_id]
+        self.client = hass.data[DOMAIN][config_entry.entry_id]
+        self.config_entry = config_entry
         self.hass = hass
-        self.raise_completed_events = entry.data[CONF_EVENT_COMPLETE]
-        self.raise_added_events = entry.data[CONF_EVENT_ADDED]
-        self.raise_removed_events = entry.data[CONF_EVENT_REMOVED]
-        self.raise_removed_events = entry.data[CONF_EVENT_REMOVED]
-
-        async_track_time_interval(hass, self.raise_events, timedelta(seconds = entry.data[CONF_EVENT_SCAN_INTERVAL]))
 
         async def service_get_torrent_info(call: ServiceCall) -> ServiceResponse:
             try:
@@ -60,19 +54,18 @@ class QBEventsAndServices:
         hass.services.async_register(DOMAIN, SERVICE_RESUME_DOWLOADS, service_resume_downloads)
         hass.services.async_register(DOMAIN, SERVICE_SHUTDOWN, service_shutdown)
 
-
     async def raise_events(self, hass):
         """Review current torrent info and determine if any events should be raised"""
         completed_torrents, added_torrents, removed_torrents = await self.hass.async_add_executor_job(compare_torrents, self.client)
 
         #Has anything changed that we care about?  Raise events as required
-        if completed_torrents and self.raise_completed_events:
-            self.hass.bus.async_fire(EVENT_COMPLETED, completed_torrents)
+        if completed_torrents and self.config_entry.options.get(CONF_EVENT_COMPLETE, DEFAULT_EVENT_COMPLETE):
+             self.hass.bus.async_fire(EVENT_COMPLETED, completed_torrents)
 
-        if added_torrents and self.raise_added_events:
+        if added_torrents and self.config_entry.options.get(CONF_EVENT_ADDED, DEFAULT_EVENT_ADDED):
             self.hass.bus.async_fire(EVENT_ADDED, added_torrents)
 
-        if removed_torrents and self.raise_removed_events:
+        if removed_torrents and self.config_entry.options.get(CONF_EVENT_REMOVED, DEFAULT_EVENT_REMOVED):
             self.hass.bus.async_fire(EVENT_REMOVED, removed_torrents)
 
         return
