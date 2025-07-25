4d6c87a9899292f10a2bacd6868c05e233745338
Andy Stewart <andy.stewart@live.com>
2025-07-25 10:35:53 +1000

    Update const.py
    Changed SERVICE_ constant names to ACTION_

--- const.py
+++ const.py
@@ -5,16 +5,8 @@ import voluptuous as vol
 import logging
 
 DOMAIN: Final = "qbittorrent_mkii"
-CONF_EVENT_COMPLETE = "torrent_complete"
-CONF_EVENT_ADDED = "torrent_added"
-CONF_EVENT_REMOVED = "torrent_removed"
-CONF_EVENT_SCAN_INTERVAL= "event_scan_interval"
 
-#Events
-EVENT_COMPLETED = DOMAIN + "_" + CONF_EVENT_COMPLETE
-EVENT_ADDED = DOMAIN + "_" + CONF_EVENT_ADDED
-EVENT_REMOVED = DOMAIN + "_" + CONF_EVENT_REMOVED
-
+#Defaults
 DEFAULT_NAME = "qBittorrent" #This is the base sensor and display name, that will have the target hostname added for uniqueness
 DEFAULT_URL = "http://192.168.1.1:8080"
 DEFAULT_VERIFY_SSL = False
@@ -24,9 +16,22 @@ DEFAULT_EVENT_COMPLETE = True
 DEFAULT_EVENT_ADDED = False
 DEFAULT_EVENT_REMOVED = False
 
-SERVICE_RESUME_DOWLOADS = 'service_resume_downloads'
-SERVICE_PAUSE_DOWLOADS = 'service_pause_downloads'
-SERVICE_GET_TORRENT_INFO = 'service_get_torrent_info'
-SERVICE_SHUTDOWN = 'service_shutdown'
+#Events
+CONF_EVENT_COMPLETE = "torrent_complete"
+CONF_EVENT_ADDED = "torrent_added"
+CONF_EVENT_REMOVED = "torrent_removed"
+CONF_EVENT_SCAN_INTERVAL= "event_scan_interval"
 
+EVENT_COMPLETED = DOMAIN + "_" + CONF_EVENT_COMPLETE
+EVENT_ADDED = DOMAIN + "_" + CONF_EVENT_ADDED
+EVENT_REMOVED = DOMAIN + "_" + CONF_EVENT_REMOVED
+
+#Services
+ACTION_RESUME_DOWLOADS = 'action_resume_downloads'
+ACTION_PAUSE_DOWLOADS = 'action_pause_downloads'
+ACTION_GET_TORRENT_INFO = 'action_get_torrent_info'
+ACTION_DELETE_TORRENT = 'action_delete_torrent'
+ACTION_SERVICE_SHUTDOWN = 'action_service_shutdown'
+
+#Logging
 LOGGER = logging.getLogger(__package__)
